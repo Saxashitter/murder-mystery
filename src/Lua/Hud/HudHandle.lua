1f683d0b192e1f4ecf62bba41e877e4a9a4ddf0f
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2024-09-23 17:55:15 -0500

    WIP hud edits

--- src/Lua/Hud/HudHandle.lua
+++ src/Lua/Hud/HudHandle.lua
@@ -1,6 +1,16 @@
 local hudwasmm = false
 local modname = "SAXAMM"
 
+local TR = TICRATE
+local HUD_BEGINNINGXOFF = 300*FU
+
+local MMHUD = {
+	ticker = 0,
+	xoffset = HUD_BEGINNINGXOFF,
+	lastmap = nil,
+}
+
+
 local types = {
 	{"Innocent",
 		V_GREENMAP,
@@ -18,45 +28,53 @@ local types = {
 
 local function HUD_RoleDrawer(v,p)
 	local patch = v.cachePatch("MMROLE")
+	local off = MMHUD.xoffset
 	local x = (320*FU)-(patch.width*FU)
 	local y = 0
-	v.drawScaled(x, y, FU, patch, V_SNAPTOTOP|V_SNAPTORIGHT|V_50TRANS)
+	v.drawScaled(x + off, y, FU, patch, V_SNAPTOTOP|V_SNAPTORIGHT|V_50TRANS)
 
 	if p.spectator 
 	or not (p.mo and p.mo.valid)
 	or p.mo.health == 0
-		v.drawString(320,0,
+		v.drawString(320*FU + off,
+			0,
 			"Dead",
 			V_GRAYMAP|V_SNAPTORIGHT|V_SNAPTOTOP|V_ALLOWLOWERCASE,
-			"right"
+			"fixed-right"
 		)
-		v.drawString(320,8,
+		v.drawString(320*FU + off,
+			8*FU,
 			"Killed by: TODO: player name",
 			V_SNAPTORIGHT|V_SNAPTOTOP|V_ALLOWLOWERCASE,
-			"thin-right"
+			"thin-fixed-right"
 		)
 		return
 	end
 	
 	if not (p.mm and types[p.mm.role]) then return end
 	
-	v.drawString(320,0,
+	v.drawString(320*FU + off,
+		0,
 		types[p.mm.role][1],
 		types[p.mm.role][2]|V_SNAPTORIGHT|V_SNAPTOTOP|V_ALLOWLOWERCASE,
-		"right"
+		"fixed-right"
 	)
 	for i = 3,7
 		if types[p.mm.role][i] ~= nil
-			v.drawString(320,8*(i - 2),
+			v.drawString(320*FU + off,
+				(8*(i - 2))*FU,
 				types[p.mm.role][i],
 				V_SNAPTORIGHT|V_SNAPTOTOP|V_ALLOWLOWERCASE,
-				"thin-right"
+				"thin-fixed-right"
 			)
 		end
 	end
 end
 
 local function HUD_WeaponDrawer(v,p)
+	if (p.spectator) then return end
+	
+	local slidein = MMHUD.xoffset
 	
 	local trans = 0
 	local yoffset = 0
@@ -65,52 +83,53 @@ local function HUD_WeaponDrawer(v,p)
 	local x = 45*FU
 	local y = 155*FU
 
-	v.drawScaled(x, y, FU, patch, V_SNAPTOLEFT|V_SNAPTOBOTTOM|V_50TRANS)
+	v.drawScaled(x - slidein, y, FU, patch, V_SNAPTOLEFT|V_SNAPTOBOTTOM|V_50TRANS)
 
 	if p.mm and p.mm.weapon and p.mm.weapon.valid
-		local wpn_t = MM:getWpnData(p)	
+		local wpn_t = MM:getWpnData(p)
+		
 		--Name
-		v.drawString(47,
-			155,
+		v.drawString(47*FU - slidein,
+			156*FU,
 			wpn_t.name or "Weapon",
 			V_YELLOWMAP|V_SNAPTOLEFT|V_SNAPTOBOTTOM|V_ALLOWLOWERCASE,
-			"left"
+			"fixed"
 		)
 		
 		--Tooltips
-		v.drawString(47,
-			163,
+		v.drawString(47*FU - slidein,
+			162*FU,
 			(p.mm.weapon.hidden and "Hidden..." or "Showing!"),
 			V_YELLOWMAP|V_SNAPTOLEFT|V_SNAPTOBOTTOM|V_ALLOWLOWERCASE|(p.mm.weapon.hidden and V_30TRANS or 0),
-			"thin"
+			"thin-fixed"
 		)
-		v.drawString(47,
-			171,
+		v.drawString(47*FU - slidein,
+			170*FU,
 			"[C1] - "..(p.mm.weapon.hidden and "Show" or "Hide").." weapon",
 			V_SNAPTOLEFT|V_SNAPTOBOTTOM|V_ALLOWLOWERCASE,
-			"thin"
+			"thin-fixed"
 		)
-		local y = 179
+		local y = 178
 		if wpn_t.droppable then
-			v.drawString(47,
-					y,
-					"[C2] - Drop weapon",
-					V_SNAPTOLEFT|V_SNAPTOBOTTOM|V_ALLOWLOWERCASE,
-					"thin"
-				)
+			v.drawString(47*FU - slidein,
+				y*FU,
+				"[C2] - Drop weapon",
+				V_SNAPTOLEFT|V_SNAPTOBOTTOM|V_ALLOWLOWERCASE,
+				"thin-fixed"
+			)
 			y = $+8
 		end
 		if not p.mm.weapon.hidden
-			v.drawString(47,
-				y,
+			v.drawString(47*FU - slidein,
+				y*FU,
 				"[FIRE] - Use weapon",
 				V_SNAPTOLEFT|V_SNAPTOBOTTOM|V_ALLOWLOWERCASE,
-				"thin"
+				"thin-fixed"
 			)
 		end
 		
 		--Icon
-		v.drawScaled(9*FU,
+		v.drawScaled(9*FU - slidein,
 			159*FU,
 			FU,
 			v.cachePatch(wpn_t.icon or "MISSING"), --v.cachePatch("MISSING"),
@@ -120,15 +139,15 @@ local function HUD_WeaponDrawer(v,p)
 		yoffset = -(p.mm.weapon.cooldown*FU)/2
 		--yoffset = ease.outquad(FU/3,$,0)
 	else
-		v.drawString(47,
-			155,
-			"No Weapon...",
-			V_30TRANS|V_GRAYMAP|V_SNAPTOLEFT|V_SNAPTOBOTTOM|V_ALLOWLOWERCASE,
-			"thin"
+		v.drawString(47*FU - slidein,
+			156*FU,
+			"No Weapon",
+			V_GRAYMAP|V_SNAPTOLEFT|V_SNAPTOBOTTOM|V_ALLOWLOWERCASE,
+			"thin-fixed"
 		)	
 	end
 	
-	v.drawScaled(5*FU,
+	v.drawScaled(5*FU - slidein,
 		155*FU + yoffset,
 		FU*2,
 		v.cachePatch("CURWEAP"),
@@ -169,8 +188,20 @@ addHook("HUD",function(v,p)
 			customhud.SetupItem("saxamm_weapontime", modname, HUD_TimeForWeapon, "game")
 		end
 		
+		MMHUD.ticker = $+1
+		if abs(leveltime - MMHUD.ticker) >= 4
+			MMHUD.ticker = leveltime
+		end
+		
+		if MMHUD.ticker >= TR*3/2
+			MMHUD.xoffset = ease.inquart(FU*9/10,$,0)
+		end
+		
 		hudwasmm = true
 	else
+		MMHUD.ticker = 0
+		MMHUD.xoffset = HUD_BEGINNINGXOFF
+		
 		if hudwasmm
 			customhud.SetupItem("rings",				"vanilla")
 			customhud.SetupItem("time",					"vanilla")
@@ -182,3 +213,8 @@ addHook("HUD",function(v,p)
 		hudwasmm = false
 	end
 end,"game")
+
+addHook("MapLoad",do
+	MMHUD.ticker = 0
+	MMHUD.xoffset = HUD_BEGINNINGXOFF
+end)

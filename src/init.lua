e659cff1febc163c9780eeeb950eb018f3d5b565
Saxashitter <nicholas.rickys3@gmail.com>
2024-09-25 04:58:04 -0400

    Added metatable to reference MM_N within MM

--- src/init.lua
+++ src/init.lua
@@ -23,3 +23,24 @@ dofile "Console/main"
 dofile "Hooks/main"
 dofile "Weapons/main"
 
+setmetatable(MM, {
+	__index = function(self, key)
+		if rawget(self, key) ~= nil then
+			return rawget(self, key)
+		end
+
+		if MM_N[key] then
+			print "net has it, return the thing from net"
+			return MM_N[key]
+		end
+	end,
+	__newindex = function(self, key, value)
+		if MM_N[key] ~= nil then
+			print "whoop we assigning shit to net now"
+			MM_N[key] = value
+			return
+		end
+
+		rawset(self, key, value)
+	end
+})

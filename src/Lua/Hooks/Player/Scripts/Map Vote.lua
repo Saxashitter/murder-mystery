2b5e0aea801642328b8c318ffccb94301fb997f7
Saxashitter <nicholas.rickys3@gmail.com>
2024-09-25 21:01:45 -0400

    Fix time limit

--- src/Lua/Hooks/Player/Scripts/Map Vote.lua
+++ src/Lua/Hooks/Player/Scripts/Map Vote.lua
@@ -3,9 +3,11 @@ local MOVE_DEADZONE = 10
 return function(p)
 	if not MM_N.gameover then return end
 
-	p.mo.momx = 0
-	p.mo.momy = 0
-	p.mo.momz = 0
+	if (p and p.mo) then
+		p.mo.momx = 0
+		p.mo.momy = 0
+		p.mo.momz = 0
+	end
 
 	if not p.mm.selected_map then
 		local moveIndex = 0

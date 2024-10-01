3142d6513068210f5643c7c127c0b8b66d63ca5b
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2024-09-30 20:58:41 -0500

    Add showdown command

--- src/Lua/Console/Commands.lua
+++ src/Lua/Console/Commands.lua
@@ -9,3 +9,10 @@ COM_AddCommand("MM_EndGame", function(p)
 
 	MM:endGame(1)
 end, COM_ADMIN)
+
+COM_AddCommand("MM_StartShowdown", function(p)
+	if not MM:isMM() then return end
+	if MM_N.showdown then return end
+	
+	MM_N.showdown = true
+end, COM_ADMIN)

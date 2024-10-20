f09e9929a6a2219247c58b5936240c6e4489f725
Blur <danielthesmartypants2017@gmail.com>
2024-10-20 00:00:30 -0500

    Murderers now have 50% chance of getting swap item

--- src/Lua/Hooks/Player/Scripts/Uno Reverse RNG.lua
+++ src/Lua/Hooks/Player/Scripts/Uno Reverse RNG.lua
@@ -3,7 +3,7 @@ local roles = MM.require "Variables/Data/Roles"
 return function(p) -- Role handler
 	if leveltime == 10*TICRATE then
 		if p.mm.role == MMROLE_MURDERER then
-			if P_RandomChance(FRACUNIT/4) then
+			if P_RandomChance(FRACUNIT/2) then
 				MM:GiveItem(p, "uno_reverse")
 			end
 		end

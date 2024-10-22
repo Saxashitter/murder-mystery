70945765d589190955274c421a4646a54ce7970b
Blur <danielthesmartypants2017@gmail.com>
2024-10-21 21:59:42 -0500

    Change gun receive message again

--- src/Lua/Hooks/Game/Manager.lua
+++ src/Lua/Hooks/Game/Manager.lua
@@ -254,10 +254,10 @@ addHook("ThinkFrame", function()
 							if play == p
 								chatprintf(play,"\x82*You have been given the gun!",true)
 							else
-								chatprintf(play,"\x82*A random player has got the gun due to inactivity!")
+								chatprintf(play,"\x82*A random player has received the gun due to inactivity!")
 							end
 						end
-						MM:discordMessage("***A random player has got the gun due to inactivity!***\n")
+						MM:discordMessage("***A random player has received the gun due to inactivity!***\n")
 						P_RemoveMobj(wpn)
 					end
 				end
@@ -270,11 +270,11 @@ addHook("ThinkFrame", function()
 					if play == p
 						chatprintf(play,"\x82*You have been given the gun!",true)
 					else
-						chatprintf(play,"\x82*A random player has got the gun due to the gun despawning!")
+						chatprintf(play,"\x82*A random player has received the gun due to the gun despawning!")
 					end
 				end
 				
-				MM:discordMessage("***A random player has got the gun due to the gun despawning!***\n")
+				MM:discordMessage("***A random player has received the gun due to the gun despawning!***\n")
 			end
 		end
 	end

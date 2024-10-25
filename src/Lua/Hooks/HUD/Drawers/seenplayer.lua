4d6f8710d292c7159d28cf339fc5cb1c678f0fd1
Blur <danielthesmartypants2017@gmail.com>
2024-10-24 21:24:34 -0500

    Update seenplayer code

--- src/Lua/Hooks/HUD/Drawers/seenplayer.lua
+++ src/Lua/Hooks/HUD/Drawers/seenplayer.lua
@@ -1,6 +1,7 @@
--- PLACEHOLDER SEENPLAYER CODE
+local sglib = MM.require "Libs/sglib"
 
 local draw_name = ""
+local g_seenplayer
 local tics = 0
 
 addHook("SeenPlayer", function(player, seenplayer)
@@ -16,22 +17,39 @@ addHook("SeenPlayer", function(player, seenplayer)
 		end
 
 		draw_name = name
+		g_seenplayer = seenplayer
 		tics = 10
 
 		return false
 	end
 end)
 
-return function(v)
+return function(v,p,c)
 	if not MM:isMM() then
 		draw_name = ""
 		tics = 0
 		return
 	end
 
-	v.drawString(160, 110, draw_name, V_ALLOWLOWERCASE, "center")
+	if g_seenplayer and g_seenplayer.valid 
+	and g_seenplayer.mo and g_seenplayer.valid then
+		local g_mo = g_seenplayer.mo
+		local flip = 1 
+		
+		if P_MobjFlip(p.realmo) ~= P_MobjFlip(g_mo) then
+			flip = -1
+		end
+		
+		local to_screen = sglib.ObjectTracking(v,p,c,{ x = g_mo.x, y = g_mo.y, z = g_mo.z+(( (g_mo.height*3)/2)*flip ) })
+		
+		if to_screen and to_screen.onScreen then
+			v.drawString(to_screen.x, to_screen.y-4*FU, draw_name, V_ALLOWLOWERCASE|V_GREENMAP|V_50TRANS, "thin-fixed-center")
+		end
+	end
+	
 	tics = max(0, $-1)
 	if tics == 0 then
 		draw_name = ""
+		g_seenplayer = nil
 	end
 end

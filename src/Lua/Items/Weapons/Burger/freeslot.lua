a55552b33bfef17189440525ee2b382ca2db8dfd
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2024-10-26 15:26:46 -0500

    Fix burger sounds

--- src/Lua/Items/Weapons/Burger/freeslot.lua
+++ src/Lua/Items/Weapons/Burger/freeslot.lua
@@ -4,4 +4,7 @@ states[freeslot "S_MM_BURGER"] = {
 	tics = -1
 }
 
+sfxinfo[freeslot "sfx_burgr1"].caption = "Can I haz cheezburger pls?"
+sfxinfo[freeslot "sfx_burgr2"].caption = "Mmm cheezburger!"
+
 return S_MM_BURGER

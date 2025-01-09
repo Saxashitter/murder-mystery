290e7bbef69b56914dd4eb33ca8deb6c408130c1
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2025-01-08 20:23:41 -0600

    Fix sound bugs in Mansion2 and MilBase

--- src/Lua/Maps/LUA_I1.lua
+++
@@ -1,5 +0,0 @@
---bullshit
-addHook("LinedefExecute",function(line,mo,sec)
-	if G_BuildMapTitle(gamemap) ~= "Mil Base" then return end
-	S_StartSound(sec,sfx_mmcam0)
-end,"MM_MAPI1")
--- src/Lua/Maps/exec.lua
+++ src/Lua/Maps/exec.lua
@@ -1,4 +1,3 @@
 local dir = "Maps"
 
-dofile(dir.."/LUA_I1.lua")
 dofile(dir.."/LUA_I2.lua")

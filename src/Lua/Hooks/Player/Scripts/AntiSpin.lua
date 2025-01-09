b7d92c5518caec8124a595aa9bf37cb273b0feb3
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2025-01-08 23:14:40 -0600

    Update AntiSpin.lua

--- src/Lua/Hooks/Player/Scripts/AntiSpin.lua
+++ src/Lua/Hooks/Player/Scripts/AntiSpin.lua
@@ -1,8 +1,10 @@
 addHook("PlayerCanEnterSpinGaps",function(p)
 	if not (MM:isMM()) then return end
+	if (CV_MM.debug.value) then return end
 	return false
 end)
 
 return function(p)
+	if (CV_MM.debug.value) then return end
 	p.pflags = $ &~(PF_STARTDASH|PF_SPINNING)
 end

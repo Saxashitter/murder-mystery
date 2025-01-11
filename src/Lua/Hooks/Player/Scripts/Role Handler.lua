5bacfaa3e7e4f16a4d7eefa32d6948a373daf92e
Blur <danielthesmartypants2017@gmail.com>
2025-01-11 17:46:58 -0600

    Add tripmine to murd special item + fix item order

--- src/Lua/Hooks/Player/Scripts/Role Handler.lua
+++ src/Lua/Hooks/Player/Scripts/Role Handler.lua
@@ -7,6 +7,14 @@ return function(p) -- Role handler
 	and not p.mm.got_weapon
 		if not MM_N.dueling
 			MM:GiveItem(p, roles[p.mm.role].weapon)
+			
+			if p.mm.role == MMROLE_MURDERER then
+				if P_RandomChance(FRACUNIT/2) then
+					local giveitem = P_RandomChance(FRACUNIT/2) and "uno_reverse" or "tripmine"
+					
+					MM:GiveItem(p, giveitem)
+				end
+			end
 		else
 			MM:GiveItem(p, MM_N.duel_item)
 		end

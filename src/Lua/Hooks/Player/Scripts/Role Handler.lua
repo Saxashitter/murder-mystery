09eef371261ead62e9daa5b74af6e2a797bcab97
Blur <danielthesmartypants2017@gmail.com>
2025-01-12 14:47:42 -0600

    Murderers always get special item.

--- src/Lua/Hooks/Player/Scripts/Role Handler.lua
+++ src/Lua/Hooks/Player/Scripts/Role Handler.lua
@@ -9,11 +9,9 @@ return function(p) -- Role handler
 			MM:GiveItem(p, roles[p.mm.role].weapon)
 			
 			if p.mm.role == MMROLE_MURDERER then
-				if P_RandomChance(FRACUNIT/2) then
-					local giveitem = P_RandomChance(FRACUNIT/2) and "uno_reverse" or "tripmine"
-					
-					MM:GiveItem(p, giveitem)
-				end
+				local giveitem = P_RandomChance(FRACUNIT/2) and "uno_reverse" or "tripmine"
+				
+				MM:GiveItem(p, giveitem)
 			end
 		else
 			MM:GiveItem(p, MM_N.duel_item)

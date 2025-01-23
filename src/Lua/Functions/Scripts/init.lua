248dd8ae1c511d8468ae30ebc883107987bd9d8f
Jisk <37682565+Jiskster@users.noreply.github.com>
2025-01-23 16:23:15 -0600

    Make clues_weaponsleft lower

--- src/Lua/Functions/Scripts/init.lua
+++ src/Lua/Functions/Scripts/init.lua
@@ -104,7 +104,7 @@ return function(self, maploaded)
 	if maploaded then
 		if not MM:isMM() then return end
 		
-		MM_N.clues_weaponsleft = max(MM:countPlayers().innocents/3,1)
+		MM_N.clues_weaponsleft = max(MM:countPlayers().innocents/5,1)
 		local clue_amm = 5
 		if (mapheaderinfo[gamemap].mm_clueamount ~= nil)
 		and tonumber(mapheaderinfo[gamemap].mm_clueamount)

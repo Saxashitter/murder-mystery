666a09ec6f3381511580fff9a4b337b5145bbe6a
Blur <danielthesmartypants2017@gmail.com>
2025-01-25 22:53:02 -0600

    more breakingg

--- src/Lua/Hooks/Player/Scripts/PlayerRework.lua
+++ src/Lua/Hooks/Player/Scripts/PlayerRework.lua
@@ -81,7 +81,7 @@ return function(p)
 	if p.powers[pw_carry] ~= CR_ZOOMTUBE
 		if p.mo.playergroundcap ~= nil then
 			if not P_IsObjectOnGround(p.mo) then
-				speedcap = min(p.mo.playergroundcap, $) or $
+				--speedcap = min(p.mo.playergroundcap, $) or $
 			end
 		end
 		

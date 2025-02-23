e66d0cce195aec5b2131e3fec92897da2da3e9c8
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2025-02-23 17:13:29 -0600

    we need to find a good way to activate 2nd perk

--- src/Lua/Perks/main.lua
+++ src/Lua/Perks/main.lua
@@ -13,6 +13,16 @@ the way that these are set up is:
 
 rawset(_G, "MM_PERKS",{})
 
+MM_PERKS.perkActiveDown = function(p, inSecondSlot)
+	if ((p.cmd.buttons & BT_TOSSFLAG)
+	and not (p.lastbuttons & BT_TOSSFLAG))
+	--nothing uses c3
+	and (inSecondSlot and (p.cmd.buttons & BT_CUSTOM3) or true)
+		return true
+	end
+	return false
+end
+
 dofile(path.."Ghost")
 dofile(path.."Ninja")
 dofile(path.."Swap")

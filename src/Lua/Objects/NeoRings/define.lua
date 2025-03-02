71ae59da60b6837b14c481d0a017f9d454446547
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2025-03-01 19:05:40 -0600

    add coin sound from mm2

--- src/Lua/Objects/NeoRings/define.lua
+++ src/Lua/Objects/NeoRings/define.lua
@@ -5,16 +5,6 @@ local SPARKLE_SPEED = 2
 states[S_MM_RING] = {SPR_MMCN, A|FF_ANIMATE, -1, nil, 7, 3, S_MM_RING}
 states[S_MM_RING_SPARKLE] = {SPR_MMSP, A|FF_ANIMATE, SPARKLE_FRAMES*SPARKLE_SPEED, nil, SPARKLE_FRAMES, 2, S_NULL}
 
-addHook("MobjSpawn", function(mo)
-	if mo and mo.valid
-		if not mo.override
-			mobjinfo[MT_RING].spawnstate = S_MM_RING
-			mobjinfo[MT_RING].deathstate = S_MM_RING_SPARKLE
-			mobjinfo[MT_RING].deathsound = sfx_s3k33
-			--mo.scale = $+(FU/3)
-			mo.override = true
-		end
-	elseif mo.override == true
-		mo.override = false
-	end
-end, MT_RING)
+mobjinfo[MT_RING].spawnstate = S_MM_RING
+mobjinfo[MT_RING].deathstate = S_MM_RING_SPARKLE
+mobjinfo[MT_RING].deathsound = sfx_s3k33

43329578d427fbfb335daa76872152f312029b87
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2025-03-02 00:59:38 -0600

    spawn rate adjustments

--- src/Lua/Hooks/Game/Scripts/MoneyPrinting.lua
+++ src/Lua/Hooks/Game/Scripts/MoneyPrinting.lua
@@ -1,9 +1,20 @@
 local TR = TICRATE
-local SPAWN_RATE = TR --*3/2
+local spawn_wait = 0
+
+local SPAWN_MIN = TR/2
+local SPAWN_MAX = TR*3/2
 
 return function()
-	if (leveltime % SPAWN_RATE) then return end
-	if not P_RandomChance(FU*4/5) then return end
+	if spawn_wait
+		spawn_wait = $ - 1
+		return
+	end
 	
-	MM.printMoney()
+	if P_RandomChance(FU*4/5)
+		MM.printMoney()
+	end
+	
+	spawn_wait = P_RandomRange(SPAWN_MIN,SPAWN_MAX)
 end
+
+addHook("NetVars",function(n) spawn_wait = n($) end)

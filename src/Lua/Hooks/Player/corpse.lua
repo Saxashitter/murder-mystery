4a640939323348e97e53a131b5b732696a096e1e
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2025-04-18 17:20:48 -0500

    no spectators finding corpses

--- src/Lua/Hooks/Player/corpse.lua
+++ src/Lua/Hooks/Player/corpse.lua
@@ -382,6 +382,7 @@ addHook("ThinkFrame", function()
 				continue
 			end
 			if p.mm.role == MMROLE_MURDERER then continue end
+            if (p.spectator) then continue end
 			if MM_N.gameover then break end
 			
 			if MM_N.knownDeadPlayers[corpse.playerid]

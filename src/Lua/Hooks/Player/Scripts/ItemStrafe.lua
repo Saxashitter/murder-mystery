2d3859f35074e822d3532b033d2fab59f4822524
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2025-04-21 15:40:32 -0500

    Fix itemstrafe?

--- src/Lua/Hooks/Player/Scripts/ItemStrafe.lua
+++ src/Lua/Hooks/Player/Scripts/ItemStrafe.lua
@@ -1,17 +1,27 @@
+local function ApplyStrafe(p)
+	--no PF_FORCESTRAFE since that turns camera turn buttons
+	--into strafe movement buttons (we want to keep old behavior)
+	p.pflags = $ &~(PF_ANALOGMODE|PF_DIRECTIONCHAR)
+	p.drawangle = p.cmd.angleturn << 16
+end
+local function NoStrafe(p)
+	p.pflags = $ &~(PF_FORCESTRAFE|PF_ANALOGMODE)
+end
+
 return function(player)
 	if player and player.valid and player.mm and player.mo and player.mo.valid then
 		local item = player.mm.inventory.items[player.mm.inventory.cur_sel]
 		
 		if not (item) then
-			player.pflags = $ | PF_DIRECTIONCHAR & ~(PF_FORCESTRAFE|PF_ANALOGMODE)
+			NoStrafe(player)
 			return 
 		end
 		
 		if item.nostrafe or player.mm.inventory.hidden then
-			player.pflags = $ | PF_DIRECTIONCHAR & ~(PF_FORCESTRAFE|PF_ANALOGMODE)
+			NoStrafe(player)
 			return
 		end
 		
-		player.pflags = $ | PF_FORCESTRAFE & ~(PF_ANALOGMODE|PF_DIRECTIONCHAR)
+		ApplyStrafe(player)
 	end
 end

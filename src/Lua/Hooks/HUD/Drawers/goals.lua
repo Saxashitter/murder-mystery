fa35ec248db1cc465cb618b7d872a6bc11877f67
luigi budd <90123055+luigi-budd@users.noreply.github.com>
2025-05-25 18:41:11 -0500

    Windows Eleven

--- src/Lua/Hooks/HUD/Drawers/goals.lua
+++ src/Lua/Hooks/HUD/Drawers/goals.lua
@@ -1,37 +1,62 @@
 --there could be a better way to do this
 local function HUD_GoalDrawer(v,p)
-	local x = 5*FU - MMHUD.xoffset
-	local y = 37*FU
+	local x = 7*FU - MMHUD.xoffset
+	local y = 40*FU
 	local flags = V_SNAPTOTOP|V_SNAPTOLEFT|V_ALLOWLOWERCASE|V_PERPLAYER
 	
 	if MM_N.dueling then return end
 	
+	local goals = {}
+	
 	if p.mm.clues.startamount ~= nil then
 		if p.mm.clues.startamount ~= 0
 		and p.mm.role ~= MMROLE_SHERIFF
-			local complete = #p.mm.clues.list == 0 and V_YELLOWMAP or 0
-			v.drawString(x,
-				y,
-				(#p.mm.clues.list).."/"..p.mm.clues.startamount.." Clues left",
-				flags|complete,
-				"thin-fixed"
-			)
-			y = $+8*FU
+			local complete = #p.mm.clues.list == 0 and V_GREENMAP or 0
+			table.insert(goals,{
+				string = (#p.mm.clues.list).."/"..p.mm.clues.startamount.." Clues left",
+				flags = flags|complete
+			})
 		end
 	end
 	
 	if p.mm.role == MMROLE_MURDERER
 	and MM_N.minimum_killed > 0
-		local complete = MM_N.peoplekilled >= MM_N.minimum_killed and V_YELLOWMAP or 0
+		local complete = MM_N.peoplekilled >= MM_N.minimum_killed and V_GREENMAP or 0
 		local needed = MM_N.numbertokill
-		v.drawString(x,
-			y,
-			MM_N.peoplekilled.."/"..needed.." People killed",
-			flags|complete,
-			"thin-fixed"
-		)
-		y = $+8*FU
+		table.insert(goals,{
+			string = MM_N.peoplekilled.."/"..needed.." People killed",
+			flags = flags|complete
+		})
 	end
 	
+	
+	if #goals
+		local longest_str = 0
+		for k,val in ipairs(goals)
+			longest_str = max(
+				v.stringWidth(val.string,0,"thin"),
+				$
+			)
+		end
+		local height = (#goals * 8)
+		
+		v.drawFill((x/FU) - 1, (y/FU) - 3,
+			longest_str + 2, 1,
+			31|V_50TRANS|(flags &~V_ALLOWLOWERCASE)
+		)
+		v.drawFill((x/FU) - 2, (y/FU) - 2,
+			longest_str + 4, height + 4,
+			31|V_50TRANS|(flags &~V_ALLOWLOWERCASE)
+		)
+		v.drawFill((x/FU) - 1, (y/FU) + height + 2,
+			longest_str + 2, 1,
+			31|V_50TRANS|(flags &~V_ALLOWLOWERCASE)
+		)
+	end
+	
+	for k,val in ipairs(goals)
+		v.drawString(x,y, val.string, val.flags, "thin-fixed")
+		y = $ + 8*FU
+	end
 end
 return HUD_GoalDrawer

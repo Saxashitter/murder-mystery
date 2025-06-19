4d1a81fcaa3db28689d97771520e214371e806ba
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2025-06-18 23:32:26 -0500

    Swap indicator for swap

--- src/Lua/Items/Weapons/Swap_Gear/def.lua
+++ src/Lua/Items/Weapons/Swap_Gear/def.lua
@@ -1,3 +1,5 @@
+local roles = MM.require "Variables/Data/Roles"
+
 local weapon = {}
 
 local MAX_COOLDOWN = TICRATE
@@ -36,6 +38,7 @@ weapon.shootmobj = MT_THOK
 weapon.equipsfx = sfx_None
 weapon.attacksfx = sfx_None
 weapon.allowdropmobj = false
+weapon.onlyhitone = true
 
 weapon.hiddenforothers = true
 weapon.cantouch = true
@@ -152,6 +155,61 @@ function weapon:onhit(player, player2)
 		P_SpawnGhostMobj(player.mo).fuse = 5
 		P_SpawnGhostMobj(player2.mo).fuse = 5
 	end
+end
+
+local function distchecks(item, p, target)
+	local dist = R_PointToDist2(p.mo.x, p.mo.y, target.x, target.y)
+	local maxdist = FixedMul(p.mo.radius + target.radius, item.range)
+
+	if dist > maxdist
+	or abs((p.mo.z + p.mo.height/2) - (target.z + target.height/2)) > FixedMul(max(p.mo.height, target.height), item.zrange or item.range)
+	or not P_CheckSight(p.mo, target) then
+		return false
+	end
+
+	--no need to check for angles if we're touchin the guy
+	if dist > p.mo.radius + target.radius
+		local adiff = FixedAngle(
+			AngleFixed(R_PointToAngle2(p.mo.x, p.mo.y, target.x, target.y)) - AngleFixed(p.cmd.angleturn << 16)
+		)
+		if AngleFixed(adiff) > 180*FU
+			adiff = InvAngle($)
+		end
+		if (AngleFixed(adiff) > 115*FU)
+			return false
+		end
+	end
+	
+	return true
+end
+
+weapon.thinker = function(item, p)
+	if not (p and p.valid) then return end
+	if p.mm.inventory.items[p.mm.inventory.cur_sel].cooldown
+		return
+	end
+	
+	for p2 in players.iterate do
+		if not (p2 ~= p
+		and p2
+		and p2.mo
+		and p2.mo.health
+		and p2.mm
+		and not p2.mm.spectator) then continue end
+		
+		local dist = R_PointToDist2(p.mo.x, p.mo.y, p2.mo.x, p2.mo.y)
+		local maxdist = FixedMul(p.mo.radius+p2.mo.radius, item.range)
+		
+		if roles[p.mm.role].team == roles[p2.mm.role].team
+		and not roles[p.mm.role].friendlyfire then
+			continue
+		end
+		
+		if not distchecks(item,p,p2.mo) then continue end
+		
+		P_SpawnLockOn(p, p2.mo, S_LOCKON1)
+	end
+
 end
 
 return weapon

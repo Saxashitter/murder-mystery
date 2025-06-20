f2e707574d6cee861d639ec0d1028aae71152b68
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2025-06-19 20:17:26 -0500

    Update wrappedadd.lua

--- src/Lua/Libs/wrappedadd.lua
+++ src/Lua/Libs/wrappedadd.lua
@@ -2,13 +2,15 @@
 return function(op1, op2)
 	--adding op2 to op1 causes an overflow
 	if (op1 + op2 < 0)
+	and (op1 > 0)
 	and (op2 > 0)
 		return INT32_MAX
 	end
 	
 	--adding op2 to op1 causes an underflow
 	if (op1 + op2 > 0)
-	and (op2 > 0)
+	and (op1 < 0)
+	and (op2 < 0)
 		return INT32_MIN
 	end
 	

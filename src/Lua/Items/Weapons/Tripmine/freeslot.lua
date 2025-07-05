ca9916ac7b47bb37c54ee60435aaae8a113e42d3
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2025-07-05 01:07:07 -0500

    Tripmines dont self inflict for consistancy

--- src/Lua/Items/Weapons/Tripmine/freeslot.lua
+++ src/Lua/Items/Weapons/Tripmine/freeslot.lua
@@ -635,7 +635,7 @@ addHook("TouchSpecial",function(mine,me)
 	--dont kill our teammates lol
 	if (p.mm.role == MMROLE_MURDERER
 	and mine.tracer and mine.tracer.player.mm.role == MMROLE_MURDERER)
-	and (me ~= mine.tracer)
+	or (me == mine.tracer)
 		mine.health = mine.info.spawnhealth
 		mine.flags = $|MF_SPECIAL|MF_SOLID
 		
